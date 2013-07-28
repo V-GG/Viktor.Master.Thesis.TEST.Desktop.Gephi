969dd62a6c1d610d2bfc4ffcae4bd993748b0a0b
ele <hipogriff@ubuntu.(none)>
2013-07-28 19:40:20 +0300

    First implementation for supporting adding/removing nodes over time.

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ import java.io.File;
 
 import javax.swing.JFrame;
 
+import org.gephi.graph.api.GraphModel;
 import org.gephi.io.importer.api.Container;
 import org.gephi.io.importer.api.ImportController;
 import org.gephi.io.processor.plugin.DefaultProcessor;
 
+import org.gephi.preview.PreviewControllerImpl;
 import org.gephi.preview.api.PreviewController;
 import org.gephi.preview.api.PreviewModel;
 import org.gephi.preview.api.PreviewProperty;
@@ -15,6 +17,8 @@ import org.gephi.preview.api.ProcessingTarget;
 import org.gephi.preview.api.RenderTarget;
 import org.gephi.preview.types.DependantOriginalColor;
 import org.gephi.project.api.ProjectController;
+import org.gephi.graph.api.Node;
+import org.gephi.graph.api.Edge;
 import org.gephi.project.api.Workspace;
 import org.openide.util.Lookup;
 
@@ -31,9 +35,10 @@ public class Main {
 		ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
 		pc.newProject();
 		Workspace workspace = pc.getCurrentWorkspace();
-		 
+		PreviewController previewController = Lookup.getDefault().lookup(PreviewController.class); 
+		
 		//Import file
-		ImportController importController = Lookup.getDefault().lookup(ImportController.class);
+		/*ImportController importController = Lookup.getDefault().lookup(ImportController.class);
 		Container container;
 		try {
 		    File file = new File("res/Java.gexf");
@@ -41,13 +46,24 @@ public class Main {
 		} catch (Exception ex) {
 		    ex.printStackTrace();
 		    return;
-		}
+		}*/
 		 
 		//Append imported data to GraphAPI
-		importController.process(container, new DefaultProcessor(), workspace);
-		 
+		//importController.process(container, new DefaultProcessor(), workspace);
+		
+		//Add nodes with code
+		GraphEditActionListener graphEditActionListener = new GraphEditActionListener(workspace, pc, previewController);
+		GraphModel gm = graphEditActionListener.GetGraphModel();
+		Node n1 = gm.factory().newNode("TEST1");
+		Node n2 = gm.factory().newNode("TEST2");
+		
+		Edge e1 = gm.factory().newEdge(n1, n2, 1f, true);
+		
+		graphEditActionListener.AddNode(n1, "Node 1");
+		graphEditActionListener.AddNode(n2, "Node 2");
+		graphEditActionListener.AddEdge(e1);
+		
 		//Preview configuration
-		PreviewController previewController = Lookup.getDefault().lookup(PreviewController.class);
 		PreviewModel previewModel = previewController.getModel();
 		previewModel.getProperties().putValue(PreviewProperty.SHOW_NODE_LABELS, Boolean.TRUE);
 		previewModel.getProperties().putValue(PreviewProperty.NODE_LABEL_COLOR, new DependantOriginalColor(Color.WHITE));
@@ -56,7 +72,8 @@ public class Main {
 		previewModel.getProperties().putValue(PreviewProperty.EDGE_RADIUS, 10f);
 		previewModel.getProperties().putValue(PreviewProperty.BACKGROUND_COLOR, Color.BLACK);
 		previewController.refreshPreview();
-		 
+		
+		
 		//New Processing target, get the PApplet
 		ProcessingTarget target = (ProcessingTarget) previewController.getRenderTarget(RenderTarget.PROCESSING_TARGET);
 		PApplet applet = target.getApplet();
@@ -68,7 +85,7 @@ public class Main {
 		target.resetZoom();
 		 
 		//Add the applet to a JFrame and display
-		JFrame frame = new JFrame("Test Preview");
+		JFrame frame = new JFrame("Viktor's Graph preview");
 		frame.setLayout(new BorderLayout());
 		 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -76,7 +93,5 @@ public class Main {
 		 
 		frame.pack();
 		frame.setVisible(true);
-
 	}
-
 }
